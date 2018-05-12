2326a4a11c295801dca536cd2c5eb56d16d7bf78
Evgen <evgen.knyzhenko@gmail.com>
2018-05-12 12:35:22 +0300

    .

--- src/Homework03.java
+++ src/Homework03.java
@@ -3,10 +3,12 @@
  */
 public class Homework03 {
     public static void main(String[] args) {
-        task_3_1();
+        //task_3_1();
+        task_3_2();
     }
 
     private static void task_3_1() {
+
         String input = "([])[({(())})]";
 
         for (int i = 0; i < input.length(); i++) {
@@ -20,5 +22,32 @@ public class Homework03 {
         } else {
             System.out.println("input isn,t correct");
         }
+    }
+
+    //=================================
+
+    private static void task_3_2() {
+
+        StringBuilder input = new StringBuilder("//a/..////b/./c//.//");
+
+        int i;
+
+        while ((i = input.indexOf("/./")) != -1) {
+            input.delete(i, i + 2);
+        }
+
+        while ((i = input.indexOf("/../")) != -1) {
+            input.delete(i - 2, i + 3);
+        }
+
+        while ((i = input.indexOf("//")) != -1) {
+            input.delete(i, i + 1);
+        }
+
+        while (input.charAt(input.length() - 1) == '/') {
+            input.delete(input.length() - 1, input.length());
+        }
+
+        System.out.println(input);
     }
 }
